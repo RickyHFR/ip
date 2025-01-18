3430472f8f51bae9d5ab3e7a674d717e5e95fc40
RickyHFR <huangfengr@gmail.com>
2025-01-18 16:55:24 +0800

    add list

--- src/main/java/Ricky.java
+++ src/main/java/Ricky.java
@@ -6,12 +6,26 @@ public class Ricky {
         String greetingMsg1 = "Hello! I'm Ricky.\n";
         String greetingMsg2 = "What can I do for you?\n";
         String greetingMsg3 = "Bye. Hope to see you again soon!\n";
+        String listHead = "added: ";
         System.out.println(greetingLine + greetingMsg1 + greetingMsg2 + greetingLine);
+        String[] taskList = new String[100];
+        int currPosition = 0;
 
         Scanner scanner = new Scanner(System.in);
         String usrRequest = scanner.nextLine();
         while (!usrRequest.equals("bye")) {
-            System.out.println(greetingLine + usrRequest + "\n" + greetingLine);
+            if (usrRequest.equals("list")) {
+                System.out.println(greetingLine);
+                for (int i = 0; i < currPosition; i++) {
+                    System.out.printf("%d. %s\n", i, taskList[i]);
+                }
+                System.out.println(greetingLine);
+                usrRequest = scanner.nextLine();
+                continue;
+            }
+            System.out.println(greetingLine + listHead + usrRequest + "\n" + greetingLine);
+            taskList[currPosition] = usrRequest;
+            currPosition++;
             usrRequest = scanner.nextLine();
         }
         System.out.println(greetingLine + greetingMsg3 + greetingLine);
