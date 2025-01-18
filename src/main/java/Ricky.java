6eccece34408d2bdaac4cc4a8c01f7f31a2c0f6f
RickyHFR <huangfengr@gmail.com>
2025-01-18 16:30:44 +0800

    Rename chatbot and add basic greeting

---
+++ src/main/java/Ricky.java
@@ -0,0 +1,9 @@
+public class Ricky {
+    public static void main(String[] args) {
+        String greetingLine = "_______________________________________________________\n";
+        String greetingMsg1 = "Hello! I'm Ricky.\n";
+        String greetingMsg2 = "What can I do for you?\n";
+        String greetingMsg3 = "Bye. Hope to see you again soon!\n";
+        System.out.println(greetingLine + greetingMsg1 + greetingMsg2 + greetingLine + greetingMsg3 + greetingLine);
+    }
+}
