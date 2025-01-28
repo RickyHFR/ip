ed5b30d3b51119fd39a3f7a521631e27e58bfcb6
RickyHFR <huangfengr@gmail.com>
2025-01-28 11:41:42 +0800

    Add testing files

--- src/main/java/ricky/Parser.java
+++ src/main/java/ricky/Parser.java
@@ -13,51 +13,51 @@ public class Parser {
         String[] inputs = input.split(" ");
         String command = inputs[0];
         switch (command) {
-            case "bye":
-                return new ExitCommand();
-            case "list":
-                return new ListCommand();
-            case "mark":
-                if (inputs.length == 1) {
-                    throw new RickyException("OOPS!!! Please specify the task number.");
-                }
-                return new MarkCommand(Integer.parseInt(inputs[1]), true);
-            case "unmark":
-                if (inputs.length == 1) {
-                    throw new RickyException("OOPS!!! Please specify the task number.");
-                }
-                return new MarkCommand(Integer.parseInt(inputs[1]), false);
-            case "delete":
-                if (inputs.length == 1) {
-                    throw new RickyException("OOPS!!! Please specify the task number to delete.");
-                }
-                return new DeleteCommand(Integer.parseInt(inputs[1]));
-            case "todo":
-                if (inputs.length == 1) {
-                    throw new RickyException("OOPS!!! The description of a todo cannot be empty.");
-                }
-                return new AddCommand(new ToDo(input.substring(5)));
-            case "deadline":
-                String[] deadlineInputs = input.substring(9).split(" /by ");
-                LocalDateTime by;
-                try {
-                    by = LocalDateTime.parse(deadlineInputs[1], DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm"));
-                } catch (Exception e) {
-                    throw new RickyException("Please follow the format: yyyy-mm-dd-HHmm");
-                }
-                return new AddCommand(new Deadline(deadlineInputs[0], by));
-            case "event":
-                String[] eventInputs = input.substring(6).split(" /from | /to ");
-                LocalDateTime from, to;
-                try {
-                    from = LocalDateTime.parse(eventInputs[1], DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm"));
-                    to = LocalDateTime.parse(eventInputs[2], DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm"));
-                } catch (Exception e) {
-                    throw new RickyException("Please follow the format: yyyy-mm-dd-HHmm");
-                }
-                return new AddCommand(new Event(eventInputs[0], from, to));
-            default:
-                return new InvalidCommand();
+        case "bye":
+            return new ExitCommand();
+        case "list":
+            return new ListCommand();
+        case "mark":
+            if (inputs.length == 1) {
+                throw new RickyException("OOPS!!! Please specify the task number.");
+            }
+            return new MarkCommand(Integer.parseInt(inputs[1]), true);
+        case "unmark":
+            if (inputs.length == 1) {
+                throw new RickyException("OOPS!!! Please specify the task number.");
+            }
+            return new MarkCommand(Integer.parseInt(inputs[1]), false);
+        case "delete":
+            if (inputs.length == 1) {
+                throw new RickyException("OOPS!!! Please specify the task number to delete.");
+            }
+            return new DeleteCommand(Integer.parseInt(inputs[1]));
+        case "todo":
+            if (inputs.length == 1) {
+                throw new RickyException("OOPS!!! The description of a todo cannot be empty.");
+            }
+            return new AddCommand(new ToDo(input.substring(5)));
+        case "deadline":
+            String[] deadlineInputs = input.substring(9).split(" /by ");
+            LocalDateTime by;
+            try {
+                by = LocalDateTime.parse(deadlineInputs[1], DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm"));
+            } catch (Exception e) {
+                throw new RickyException("Please follow the format: yyyy-mm-dd-HHmm");
+            }
+            return new AddCommand(new Deadline(deadlineInputs[0], by));
+        case "event":
+            String[] eventInputs = input.substring(6).split(" /from | /to ");
+            LocalDateTime from, to;
+            try {
+                from = LocalDateTime.parse(eventInputs[1], DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm"));
+                to = LocalDateTime.parse(eventInputs[2], DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm"));
+            } catch (Exception e) {
+                throw new RickyException("Please follow the format: yyyy-mm-dd-HHmm");
+            }
+            return new AddCommand(new Event(eventInputs[0], from, to));
+        default:
+            return new InvalidCommand();
         }
     }
 }
---
+++ src/test/java/ricky/task/DeadlineTest.java
@@ -0,0 +1,23 @@
+package ricky.task;
+
+import org.junit.jupiter.api.Test;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DeadlineTest {
+    @Test
+    public void testToString() {
+        LocalDateTime by = LocalDateTime.parse("2023-12-31T23:59", DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+        Deadline deadline = new Deadline("submit report", by);
+        assertEquals("[D][ ] submit report (by: Dec 31 2023 11:59PM)", deadline.toString());
+    }
+
+    @Test
+    public void testStore() {
+        LocalDateTime by = LocalDateTime.parse("2023-12-31T23:59", DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+        Deadline deadline = new Deadline("submit report", by);
+        assertEquals("D | 0 | submit report | 2023-12-31T23:59", deadline.store());
+    }
+}
