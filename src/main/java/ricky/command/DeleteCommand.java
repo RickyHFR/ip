fcb50180806ea5380cb90780e41203ff0f51dce9
RickyHFR <huangfengr@gmail.com>
2025-02-21 22:50:48 +0800

    Fix small buts in DeleteCommand
    - Fix typo in DeleteCommand

--- src/main/java/ricky/command/DeleteCommand.java
+++ src/main/java/ricky/command/DeleteCommand.java
@@ -34,7 +34,7 @@ public class DeleteCommand extends Command {
     public String execute(TaskList tasks, Ui ui, Storage storage) throws RickyException {
         assert index > 0 : "Index should be greater than 0";
         assert index <= tasks.size() : "Index should be less than or equal to the number of tasks";
-        if (tasks.size() <= tasks.size()) {
+        if (index <= tasks.size()) {
             throw new RickyException("The task index is out of range.");
         }
         String output = ui.getDeleteMessage(tasks.get(index - 1), tasks);
